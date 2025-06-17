1be56765ea97c661c86747f454e98b9cace31b4c
swawiye <sovereign.wawiye@gmail.com>
2025-06-17 09:30:03 +0300

    time validation

--- organs.py
+++ organs.py
@@ -7,12 +7,21 @@ print("Welcome to the organ donation system.")
 # Date validation
 def validate_date(prompt):
     while True:
-        date_input = input(prompt).strip()
+        date_input = input(prompt).strip() # user input
         try:
             datetime.strptime(date_input, "%d/%m/%Y") #regex for date format
             return date_input
         except ValueError:
             print("Invalid date format. Please use DD/MM/YYY.")
+
+# Time validation
+def validate_time(prompt):
+    while True:
+        time_input = input(prompt).strip() # user input
+        if re.match(r"^(?:[01]\d|2[0-3]):[0-5]\d$", time_input):
+            return time_input
+        else:
+            print("Invalid time format. Please use HH:MM (24-hour format).")
 
 
 while True:
@@ -30,7 +39,7 @@ while True:
         state = input("4. State (e.g., dead or alive): ")
         hospital = input("5. Hospital (e.g., Aga Khan): ")
         donation_date = validate_date("6. Date of donation (DD/MM/YYYY): ")
-        surgery_time = input("7. Time the surgery was completed: ")
+        surgery_time = validate_time("7. Time the surgery was completed(HH:MM - 24-hour format): ")
         org = input("8. Donation Organization: ")
 
         print("\nThank you! The donor information has been recorded.")
@@ -44,7 +53,7 @@ while True:
         print("Hospital: " + hospital)
         print("Donation Date: " + donation_date)
         print("Time Surgery Was Completed: " + surgery_time)
-        print("Donation Organization: " + org)
+        print("Donation Organization: " + org + "\n")
         break
 
     elif position == '2':
@@ -56,7 +65,7 @@ while True:
         hospital = input("4. Hospital (e.g., Aga Khan): ")
         donor_id = input("5. Donor ID: ")
         reception_date = validate_date("6. Date of reception (DD/MM/YYYY): ")
-        surgery_time = input("7. Time the surgery is set to begin: ")
+        surgery_time = validate_time("7. Time the surgery is set to begin (HH:MM - 24-hour format): ")
         org = input("8. Donation Organization: ")
 
         print("\nThank you! The recipient information has been recorded.")
@@ -70,7 +79,7 @@ while True:
         print("Donor ID: " + donor_id)
         print("Reception Date: " + reception_date)
         print("Time Surgery Is Set to Begin: " + surgery_time)
-        print("Donation Organization: " + org)
+        print("Donation Organization: " + org + "\n")
         break
 
     else:
