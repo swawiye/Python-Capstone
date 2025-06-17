9e2b9708243be8fbfa0bb141a58a1e19165f2fae
swawiye <sovereign.wawiye@gmail.com>
2025-06-17 08:06:43 +0300

    initailization logic

---
+++ organs.py
@@ -0,0 +1,27 @@
+print("==== ORGAN DONATION SYSTEM ====")
+print("Welcome to the organ donation system.")
+
+input("\nAre you an organ donor or an organ recepient?(ex: organ donor) ")
+
+# Organ Donors
+print("\nEnter your details")
+input("1. Full Name(ex: Maria Talasow Carter): ")
+input("2. Nation where you are currently residing: ")
+input("3. D.O.B(DD/MM/YYYY): ")
+input("4. State(ex: either dead or alive): ")
+input("5. Hospital(ex: Aga Khan): ")
+input("6. Date of donation(DD/MM/YYYY): ")
+input("7. Time the surgery was completed: ")
+input("8. Donation Organisation: ")
+
+# Organ Recepients
+print("\nEnter your details")
+input("1. Full Name(ex: Maria Talasow Carter): ")
+input("2. Nation where you are currently residing: ")
+input("3. D.O.B(DD/MM/YYYY): ")
+input("4. Hospital(ex: Aga Khan): ")
+input("5. Donor ID: ")
+input("6. Date of reception(DD/MM/YYYY): ")
+input("7. Time the surgery is set to begin: ")
+input("8. Donation Organisation: ")
+
