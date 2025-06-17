5dc45a5bf1bb5c57fbfccf0da58208527b580183
swawiye <sovereign.wawiye@gmail.com>
2025-06-17 09:11:58 +0300

    date validation

--- organs.py
+++ organs.py
@@ -1,5 +1,19 @@
+import re # regex module
+from datetime import datetime # importing the datetime class
+
 print("==== ORGAN DONATION SYSTEM ====")
 print("Welcome to the organ donation system.")
+
+# Date validation
+def validate_date(prompt):
+    while True:
+        date_input = input(prompt).strip()
+        try:
+            datetime.strptime(date_input, "%d%m%Y")
+            return date_input
+        except ValueError:
+            print("Invalid date format. Please use DD/MM/YYY.")
+
 
 while True:
     print("\nAre you an organ donor or an organ recipient?")
@@ -12,10 +26,10 @@ while True:
         print("\nEnter your details:")
         full_name = input("1. Full Name (e.g., Maria Talasow Carter): ")
         nation = input("2. Nation where donor is currently residing: ")
-        dob = input("3. D.O.B (DD/MM/YYYY): ")
+        dob = validate_date("3. D.O.B (DD/MM/YYYY): ")
         state = input("4. State (e.g., dead or alive): ")
         hospital = input("5. Hospital (e.g., Aga Khan): ")
-        donation_date = input("6. Date of donation (DD/MM/YYYY): ")
+        donation_date = validate_date("6. Date of donation (DD/MM/YYYY): ")
         surgery_time = input("7. Time the surgery was completed: ")
         org = input("8. Donation Organization: ")
 
@@ -38,10 +52,10 @@ while True:
         print("\nEnter your details:")
         full_name = input("1. Full Name (e.g., Maria Talasow Carter): ")
         nation = input("2. Nation where recipient is currently residing: ")
-        dob = input("3. D.O.B (DD/MM/YYYY): ")
+        dob = validate_date("3. D.O.B (DD/MM/YYYY): ")
         hospital = input("4. Hospital (e.g., Aga Khan): ")
         donor_id = input("5. Donor ID: ")
-        reception_date = input("6. Date of reception (DD/MM/YYYY): ")
+        reception_date = validate_date("6. Date of reception (DD/MM/YYYY): ")
         surgery_time = input("7. Time the surgery is set to begin: ")
         org = input("8. Donation Organization: ")
 
